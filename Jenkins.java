c7c830241a79d288fd5bbee5ca91ddf9407ec5b5
deepskingdom <deepskingdom@gmail.com>
2019-01-24 18:07:56 +1100

    Initial commit

---
+++ Jenkins.java
@@ -0,0 +1,13 @@
+package myJAVA;
+
+public class Jenkins {
+
+	public static void main(String[] args) {
+		
+		String array[]= {"Ammu","Nikil","Agni"};
+		for(int i=0;i<3;i++) {
+		
+		System.out.println(array[i]+", I love you... ");
+	}
+	}	
+}
